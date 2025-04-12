a80a3593eceda5482a3dcdf97900c1a1a70dc7a1
CakraSera <rakelsera2001@gmail.com>
2025-04-12 13:36:54 +0700

    refactor: enhance output formatting in searchContact and displayContacts functions

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ function searchContact(name = "Unknown") {
   const userAvailable = contacts.find((contact) =>
     contact.fullName.includes(name)
   );
-  if (userAvailable) console.log("Contact found");
-  console.log(
-    `${userAvailable.fullName} (${
-      userAvailable.phones[0].number ?? userAvailable.phones[1].mobile
-    }) ${
-      userAvailable.emails[0].address ?? userAvailable.emails[1].address
-    } in ${userAvailable.locations[0].city}, ${
-      userAvailable.locations[0].country
-    }`
-  );
+  if (userAvailable) {
+    console.log("contact found");
+    console.log("=========================================");
+    console.log(`Name: ${userAvailable.fullName}`);
+    console.log(
+      `Phone: ${
+        userAvailable.phones[0].number ?? userAvailable.phones[1].mobile
+      }`
+    );
+    console.log(`Email: ${userAvailable.emails[0].address}`);
+    console.log(`City: ${userAvailable.locations[0].city}`);
+    console.log(`Country: ${userAvailable.locations[0].country}`);
+    console.log("=========================================");
+  }
 }
 
 function displayContacts() {
   contacts.map((contact) => {
-    console.log(
-      `${contact.fullName} (${
-        contact.phones[0].number ?? contact.phones[1].mobile
-      }) ${contact.emails[0].address ?? contact.emails[1].address} in ${
-        contact.locations[0].city
-      }, ${contact.locations[0].country}`
-    );
+    console.log("=========================================");
+    console.log(`Name: ${contact.fullName}`);
+    console.log(`Phone: ${contact.phones[0].number}`);
+    console.log(`Email: ${contact.emails[0].address}`);
+    console.log(`City: ${contact.locations[0].city}`);
+    console.log(`Country: ${contact.locations[0].country}`);
+    console.log("=========================================");
   });
 }
 
-displayContacts();
+// displayContacts();
 searchContact("David");
