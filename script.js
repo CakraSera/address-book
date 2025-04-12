f6ae53c528fac6d420a12e92ffde14a190ff2cf4
CakraSera <rakelsera2001@gmail.com>
2025-04-12 13:26:22 +0700

    refactor: streamline searchContact function and improve listingContacts implementation

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ function searchContact(name = "Unknown") {
   const userAvailable = contacts.find((contact) =>
     contact.fullName.includes(name)
   );
-  if (userAvailable)
-    console.log(
-      `${userAvailable.fullName} (${
-        userAvailable.phones[0].number ?? userAvailable.phones[1].mobile
-      }) ${
-        userAvailable.emails[0].address ?? userAvailable.emails[1].address
-      } in ${userAvailable.locations[0].city}, ${
-        userAvailable.locations[0].country
-      }`
-    );
+  if (userAvailable) console.log("Contact found");
+  console.log(
+    `${userAvailable.fullName} (${
+      userAvailable.phones[0].number ?? userAvailable.phones[1].mobile
+    }) ${
+      userAvailable.emails[0].address ?? userAvailable.emails[1].address
+    } in ${userAvailable.locations[0].city}, ${
+      userAvailable.locations[0].country
+    }`
+  );
 }
 
+function listingContacts() {
+  contacts.map((contact) => {
+    console.log(
+      `${contact.fullName} (${
+        contact.phones[0].number ?? contact.phones[1].mobile
+      }) ${contact.emails[0].address ?? contact.emails[1].address} in ${
+        contact.locations[0].city
+      }, ${contact.locations[0].country}`
+    );
+  });
+}
+
+listingContacts();
 searchContact("David");
