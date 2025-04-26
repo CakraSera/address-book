0be51c2ba7cc0dc5577b81fbcb9193f0f48234ba
CakraSera <rakelsera2001@gmail.com>
2025-04-26 18:48:46 +0700

    feat: Enhance search functionality and update contact rendering in the address book

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import { storageContacts } from "./storage.js";
 
 const allContactList = document.getElementById("all-contact-list");
+const searchInput = document.getElementById("search-input");
+const viewContactButton = document.getElementById("view-contact");
+const editContactButton = document.getElementById("edit-contact");
+const deleteContactButtons = document.querySelectorAll("#delete-contact");
 
 saveContactsToLocalStorage(storageContacts);
 const contacts = getContactsFromLocalStorage();
@@ -8,19 +12,6 @@ const contacts = getContactsFromLocalStorage();
 function renderContacts() {
   allContactList.innerHTML = contacts
     .map((contact) => {
-      // return `<li>
-      //         <div class="mb-4 card bg-gray-300 w-96 shadow-sm">
-      //           <div>
-      //             <h3>${contact.fullName}</h3>
-      //             <p>Phone: ${contact.phones[0].number}</p>
-      //             <p>Email: ${contact.emails[0].address}</p>
-      //           </div>
-      //           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-      //             <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-      //           </svg>
-      //         </div>
-      //       </li>`;
-
       return ` <tr>
                 <td>
                   <div class="flex items-center gap-3">
@@ -58,9 +49,62 @@ function renderContacts() {
                   </ul>
                 </td>
                 <th>
-                  <button class="btn btn-ghost btn-xs">view</button>
-                  <button class="btn btn-ghost btn-xs">edit</button>
-                  <button class="btn btn-ghost btn-xs">delete</button>
+                  <button id="view-contact" class="btn btn-ghost btn-xs">view</button>
+                  <button id="edit-contact" class="btn btn-ghost btn-xs">edit</button>
+                  <button id="delete-contact" class="btn btn-ghost btn-xs" data-id=${
+                    contact.id
+                  }>delete</button>
+                </th>
+              </tr>`;
+    })
+    .join("");
+}
+
+function renderContactsv2(foundContacts) {
+  allContactList.innerHTML = foundContacts
+    .map((contact) => {
+      return ` <tr>
+                <td>
+                  <div class="flex items-center gap-3">
+                    <div class="avatar">
+                      <div class="mask mask-squircle h-12 w-12">
+                        <img
+                          src=${contact.photoUrl}
+                          alt="Avatar Tailwind CSS Component" />
+                      </div>
+                    </div>
+                    <div>
+                      <div class="font-bold">${contact.fullName}</div>
+                      <div class="text-sm opacity-50">${
+                        contact.locations[0].country
+                      }</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  ${contact.workProfile.company}
+                  <br />
+                  <span class="badge badge-ghost badge-sm"
+                    >${contact.workProfile.jobTitle}</span
+                  >
+                </td>
+                <td>+62-${contact.phones[1].number}</td>
+                <td>${contact.emails[1].address}</td>
+                <td>
+                  <ul class="flex gap-2">
+                    ${contact.groups.map((group) => {
+                      return `<li>
+                        <div class="badge badge-success">${group}</div>
+                        `;
+                    })}
+                  </ul>
+                </td>
+                <th>
+                  <button id="view-contact" class="btn btn-ghost btn-xs">view</button>
+                  <button id="edit-contact" class="btn btn-ghost btn-xs">edit</button>
+                  <button id="delete-contact" class="btn btn-ghost btn-xs" data-id=${
+                    contact.id
+                  }>delete</button>
                 </th>
               </tr>`;
     })
@@ -91,32 +135,21 @@ function getContactsFromLocalStorage() {
   }
 }
 
-function searchContacts(keyword = "Unknown") {
-  const foundContacts = contacts.filter((contact) =>
-    contact.fullName.toLowerCase().includes(keyword.toLowerCase())
-  );
-
-  if (foundContacts.length <= 0) {
-    throw Error("No contacts found");
+function searchContacts(event) {
+  console.log("🚀 ~ searchContacts ~ event:", event.key);
+  if (event.key === "Enter") {
+    const keyword = event.target.value;
+    console.log("🚀 ~ searchContacts ~ keyword:", keyword);
+    const foundContacts = contacts.filter((contact) =>
+      contact.fullName.toLowerCase().includes(keyword.toLowerCase())
+    );
+    if (foundContacts.length <= 0) {
+      throw Error("No contacts found");
+    }
+    renderContactsv2(foundContacts);
+    // return foundContacts;
   }
-
-  return foundContacts;
 }
-
-function displayContacts(contacts) {
-  contacts.forEach((contacts) => {});
-  contacts.forEach((contact) => {
-    console.log("=========================================");
-    console.log("ID:" + contact.id);
-    console.log(`Name: ${contact.fullName}`);
-    console.log(`Phone: ${contact.phones[0].number}`);
-    console.log(`Email: ${contact.emails[0].address}`);
-    console.log(`City: ${contact.locations[0].city}`);
-    console.log(`Country: ${contact.locations[0].country}`);
-    console.log("=========================================");
-  });
-}
-
 function addNewContact(contact) {
   if (!contact) {
     throw Error("Error: Data contact is required");
@@ -132,18 +165,19 @@ function addNewContact(contact) {
   saveContactsToLocalStorage(contacts);
 }
 
-function deleteDataContact(id) {
-  const contactToDelete = contacts.find((contact) => contact.id === id);
+function deleteDataContact(event) {
+  console.log("🚀 ~ deleteDataContact ~ event:", event);
+  // const contactToDelete = contacts.find((contact) => contact.id === id);
 
-  if (!contactToDelete) {
-    throw Error("Data not found");
-  }
+  // if (!contactToDelete) {
+  //   throw Error("Data not found");
+  // }
 
-  const newContacts = contacts.filter((contact) => contact.id !== id);
+  // const newContacts = contacts.filter((contact) => contact.id !== id);
 
-  saveContactsToLocalStorage(newContacts);
-
-  console.info(`Contact with name ${contactToDelete.fullName} has been delete`);
+  // saveContactsToLocalStorage(newContacts);
+  // alert(`Contact with name ${contactToDelete.fullName} has been delete`);
+  // renderContacts();
 }
 
 function editDataContact(id, formData) {
@@ -161,3 +195,11 @@ function editDataContact(id, formData) {
 }
 
 renderContacts();
+
+searchInput.addEventListener("keypress", (event) => searchContacts(event));
+
+deleteContactButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("🚀 ~ button:", button);
+  });
+});
